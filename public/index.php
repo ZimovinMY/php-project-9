1488f7add83e1a331388c82b8700e658e7cc31f1
ZimovinMY <misch.zimowin2013@mail.ru>
2025-03-03 04:55:20 +0300

    2 step v1

--- public/index.php
+++ public/index.php
@@ -1,16 +1,33 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Container;
+use Slim\Psr7\Response;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$container = new Container();
 
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello, World!");
-    return $response;
+$container->set(Twig::class, function () {
+    return Twig::create(__DIR__ . '/../templates', ['cache' => false]);
 });
+
+$app = AppFactory::createFromContainer($container);
+
+// $app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
+
+$app->addErrorMiddleware(true, true, true);
+
+// Обработчик главной страницы
+$app->get('/', function (Request $request, Response $response, $args) {
+    return $this->get(Twig::class)->render($response, 'main.html.twig');
+})->setName('main');
+
+// $app->get('/', function () {
+//     return 'index.html.twig';
+// });
 
 $app->run();
